61be210ae48f08718dac74e370a1d6ef986cd530
CH-ThomasWest <thomas.west@collectivehealth.com>
2021-03-05 11:14:14 -0800

    Removed dyn/danglers as these are not applicable to this version.

--- source/app.py
+++ source/app.py
@@ -10,28 +10,6 @@ def main():
     response = make_response('Hello World!',200)
     return response
 
-@application.route('/dyn')
-def dyninfo():
-    # ipauditor status
-    r = redis.Redis(host='redis', port=6379, db=0)
-    try:
-        json = decrypt(r.get('dynrecords'))
-        return json
-    except TypeError:
-        response = make_response("No Data Populated Yet...",200)
-        return response
-
-@application.route('/danglers')
-def dyndang():
-    # ipauditor status
-    r = redis.Redis(host='redis', port=6379, db=0)
-    try:
-        json = decrypt(r.get('danglers'))
-        return json
-    except TypeError:
-        response = make_response("No Data Populated Yet...",200)
-        return response
-
 @application.route('/ips')
 def ipinfo():
     # ipauditor status
